03cd2315496b7b1bec421d051318a6335cc4ba2f
MirEnovi <garciaivonne11@gmail.com>
2018-10-13 23:11:18 -0500

    Updates

---
+++ precache-manifest.e0af116b7bc015caa2c55bd4cc3e6468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/MirEnovi.github.io/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "9188c1d4526f75997810",
+    "url": "/MirEnovi.github.io/static/js/runtime~main.9188c1d4.js"
+  },
+  {
+    "revision": "ede41ef97e606b1b601f",
+    "url": "/MirEnovi.github.io/static/js/main.ede41ef9.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/MirEnovi.github.io/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "ede41ef97e606b1b601f",
+    "url": "/MirEnovi.github.io/static/css/main.07381a66.chunk.css"
+  },
+  {
+    "revision": "494bcedaca0b30d11d9dde9cc17fbe37",
+    "url": "/MirEnovi.github.io/index.html"
+  }
+];
