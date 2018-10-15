12b643baec9f57e7aef965d98193e6d4c72c7463
MirEnovi <garciaivonne11@gmail.com>
2018-10-15 14:24:10 -0500

    Updates

--- Readme.md
+++
@@ -1,5 +0,0 @@
-# Mir García-Portafolio. 
-### [Curriculum PDF](https://drive.google.com/file/d/1pszSBqKuQKF5FLMJTYIgpyAhkBKnsabK/view?usp=sharing).
-
-
-### [Perfil Linkedin](https://www.linkedin.com/in/miriangarcia/).
---
+++ precache-manifest.4393ccff9c3957e91332cd9e3114b66b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a880f2c6f1612010a9ed1ad2c47b95bf",
+    "url": "/static/media/react.a880f2c6.png"
+  },
+  {
+    "revision": "2532411b387e5ec17d5ce1c1c2a2eef2",
+    "url": "/static/media/nodejs.2532411b.jpg"
+  },
+  {
+    "revision": "79098214f0b341c4eb25b598cda950cb",
+    "url": "/static/media/logo2.79098214.png"
+  },
+  {
+    "revision": "8bcfaef95f0b4d36d0c13794c0b23f05",
+    "url": "/static/media/js.8bcfaef9.gif"
+  },
+  {
+    "revision": "8d2e01c357efd7bbf09ec5050c34e51e",
+    "url": "/static/media/code.8d2e01c3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b5d7d67551e1a42d4dd2",
+    "url": "/static/js/main.b5d7d675.chunk.js"
+  },
+  {
+    "revision": "2287f4fad4d962cd9783",
+    "url": "/static/js/1.2287f4fa.chunk.js"
+  },
+  {
+    "revision": "b5d7d67551e1a42d4dd2",
+    "url": "/static/css/main.25b18bd0.chunk.css"
+  },
+  {
+    "revision": "1d1ed985a874975a83fe5d7ae1a4dff4",
+    "url": "/index.html"
+  }
+];
