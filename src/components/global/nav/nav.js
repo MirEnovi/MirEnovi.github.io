9aa0bcd2b93cb9710bf117d946a9dfc5931b6049
MirEnovi <garciaivonne11@gmail.com>
2018-10-14 23:56:21 -0500

    add logo

--- src/components/global/nav/nav.js
+++ src/components/global/nav/nav.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 
 import './nav.css';
-import { Navbar, NavItem } from 'react-materialize'
+import { Navbar, NavItem } from 'react-materialize';
+
+import logo from '../../assets/logo1.png';
 
 class Nav extends Component {
 	render() {
 		return (
-			<Navbar brand='Mir García' left className = 'amber blue-grey-text text-darken-3'>
+			<Navbar brand={
+				<p className = 'text-nav'>Mirian García</p>
+				} 
+				left className = 'amber blue-grey-text text-darken-3'>
 				{/* <ul id = 'nav-mobile' > */}
 					<NavItem>
-						<a className = 'blue-grey-text text-darken-3' href='badges.html'>Acerca de Mi</a>
+						<a className = 'blue-grey-text text-darken-3 text-nav' href='badges.html'>Acerca de Mí</a>
 					</NavItem>
 					<NavItem>
-						<a className = 'blue-grey-text text-darken-3' href='badges.html'>PortafoNavItemo</a>
+						<a className = 'blue-grey-text text-darken-3 text-nav' href='badges.html'>Portafolio</a>
 					</NavItem>
 					<NavItem>
-						<a className = 'blue-grey-text text-darken-3' href='collapsible.html'>Contacto</a>
+						<a className = 'blue-grey-text text-darken-3 text-nav' href='collapsible.html'>Contacto</a>
 					</NavItem>
 				{/* </ul> */}
 			</Navbar>
