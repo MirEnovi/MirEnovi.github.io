62273ebb349369d3da293a690f7db55d78ade89c
MirEnovi <garciaivonne11@gmail.com>
2018-10-16 02:11:35 -0500

    Add aboutme and home content

--- src/components/global/aboutme/aboutme.js
+++ src/components/global/aboutme/aboutme.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import './aboutme.css';
 
-import img1 from '../../assets/logo2.png';
-import img2 from '../../assets/nodejs.jpg';
-import img3 from '../../assets/code.png';
-import img4 from '../../assets/react.png';
-import img5 from '../../assets/js.gif'
+import PhotoMir from '../../assets/PhotoMir.png';
+// import img1 from '../../assets/logo2.png';
+// import img2 from '../../assets/nodejs.jpg';
+// import img3 from '../../assets/code.png';
+// import img4 from '../../assets/react.png';
+// import img5 from '../../assets/js.gif'
 
 import { Carousel } from 'react-materialize';
 
 const Aboutme = (props) => {
 	return (
-		<section className = 'row inicio text-body'>
-			<div className = 'col s12 m5'>
-				<h2 className = 'valign-wrapper'>Bienvenido</h2>
+		<section className = 'row aboutme text-body'>
+			<div className = 'col s12 l5'>
+				<h3 className = 'valign-wrapper'>Bienvenido</h3>
 				<p className = 'text-body-size left-align'>
 					Mi nombre es <b>Mirian Ivonne García Hernández</b>.
-					Me apasiona el código.
-					Actualmente soy Front-End Developer. Hago Web App's con JavaScript, implementando herramientas como React.js, Node.js, Materialize y Bootstrap. Me forje en el código en Laboratoria, pero como el mundo no se detiene, me considero estudiante de por vida y me gusta ser autodidacta, por lo que espero agregar mas tecnologías a la lista anterior.
+					Me apasiona el código.<br/>
+					Actualmente soy Front-End Developer. Hago Web App's con JavaScript, implementando herramientas como React.js, Node.js, Materialize y Bootstrap.<br/> 
+					Me forje en el código en Laboratoria, pero como el mundo no se detiene, me considero estudiante de por vida y me gusta ser autodidacta, por lo que espero agregar mas tecnologías a la lista anterior.
 					
 				</p>
 			</div>
-			<div className = 'height col s12 m7'>
-				<Carousel
+			<div className = 'col s12 l5 offset-l1'>
+				<img src = {PhotoMir} alt = 'Mirian Garcia´s Photo' className = 'photo-mir ' />
+				{/* <Carousel
 					className ='responsive-img'
 					images={[
 						img1,
@@ -31,7 +34,12 @@ const Aboutme = (props) => {
 						img4,
 						img5
 					]} 
-				/>
+				/> */}
+				{/* <br/>
+				<br/>
+				<br/>
+				<br/> */}
+				<div class="divider"></div>
 			</div>
 		</section>
 	)
--- src/components/global/home/home.js
+++ src/components/global/home/home.js
@@ -1,7 +1,27 @@
 import React from 'react';
+// import { Carousel } from 'react-materialize';
+
+import './home.css';
+// import PhotoMir from '../../assets/PhotoMir.png';
+import img3 from '../../assets/code.png';
+import img5 from '../../assets/js.gif';
+
 
 const Home = () => {
 	return (
-		sectio
+		<section className ='background-home'>
+			<div className = 'row home'>
+				<dic className = 'col s10 m6 l4 offset-s1 offset-m1 offset-l1 background-carusel'>
+					{/* <img src = {PhotoMir} alt = 'Mirian Garcia´s Photo' className = 'photo-mir' /> */}
+					<img src = {img5} alt = 'Logo' className = 'col s12 photo-home' />
+				</dic>
+				<div className = 'col s10 m5 l5 text-home'>
+					<h2>Front-End Developer</h2>
+				</div>
+			</div>
+			<div class="divider"></div>
+		</section>
 	)
 }
+
+export default Home;
